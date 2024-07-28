b6caad5bcfb20ecefe37e4763e0d63a5585de7f2
ProfItalo18 <162839318+ProfItalo18@users.noreply.github.com>
2024-07-28 20:13:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+// Adicione qualquer funcionalidade JavaScript que você desejar aqui
+// Por exemplo, um simples alerta quando a página carrega
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Página carregada!');
+});
